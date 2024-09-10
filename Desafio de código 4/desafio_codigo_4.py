f79135f0ae5960b11e6a992cc5aaacd1a1b4740a
Carlos Jota <carlos.jota87@gmail.com>
2024-09-10 15:04:41 -0300

    adicionado desafio de código 4 melhorado

--- Desafio de código 4/desafio_codigo_4.py
+++ Desafio de código 4/desafio_codigo_4.py
@@ -4,6 +4,9 @@ class Venda:
         self.quantidade = quantidade
         self.valor = valor
 
+    def valor_total(self):
+        return self.quantidade * self.valor
+
 class Categoria:
     def __init__(self, nome):
         self.nome = nome
@@ -11,24 +14,24 @@ class Categoria:
 
     # TODOS: Implementar o método adicionar_venda para adicionar uma venda à lista de vendas:
     def adicionar_venda(self, venda):
-    # Adiciona uma venda ao relatório, se for uma instância válida da classe Venda.
+    # Adiciona uma venda a lista de vendas, se for uma instância válida da classe Venda.
         if isinstance(venda, Venda):
             self.vendas.append(venda)
     # TODOS: Implementar o método total_vendas para calcular e retornar o total das vendas
     def total_vendas(self):
-    # Calcula o total de todas as vendas no relatório.
+    # Calcula o total de todas as vendas da lista de vendas SEM CONSIDERAR A QUANTIDADE
         return sum(venda.valor for venda in self.vendas)
 
 
 def main():
     categorias = []
 
-    for i in range(2):
-        nome_categoria = input()
+    for _ in range(2):
+        nome_categoria = input("Nome da categoria: ")
         categoria = Categoria(nome_categoria)
 
-        for j in range(2): 
-            entrada_venda = input()
+        for _ in range(2): 
+            entrada_venda = input("Digite o produto, quantidade e o valor (ex: Produto, 4, 4.99)")
             produto, quantidade, valor = entrada_venda.split(',')
             quantidade = int(quantidade.strip())
             valor = float(valor.strip())
@@ -42,7 +45,8 @@ def main():
     # Exibindo os totais de vendas para cada categoria
     for categoria in categorias:
       # TODOS: Exibir o total de vendas usando o método total_vendas:
-      print(f"Vendas em {categoria.nome}: {categoria.total_vendas()}")
+      total_vendas = categoria.total_vendas()
+      print(f"Vendas em {categoria.nome}: {total_vendas:.2f}")
 
 if __name__ == "__main__":
     main()
